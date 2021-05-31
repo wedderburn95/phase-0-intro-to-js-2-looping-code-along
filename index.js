6555c45d239fd5f9c932f6cb539230f57a2ec33c
william <wedderburn95@yahoo.com>
2021-05-30 22:33:31 -0400

    commit tree ac6b318c59cde65480be4c2eebe7cfdb3e14dd98

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = [Lisa, Kaitlin, Jan]
+function writeCards( names, event ) {
+    let thankYouCards = []
+    //initial expression; condition; increment expression
+    for ( let i = 0; i < names.length; i++ ) {
+        thankYouCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown( firstNumber ) {
+    while ( firstNumber > 0 ) {
+        console.log( firstNumber );
+        firstNumber -= 1;
+    }
+    console.log( firstNumber )
+}
